7fea3106951a984d09a38b520cf4c046b9b5f78e
Yasinsivis <yasinsivis9@gmail.com>
2024-05-17 00:07:48 +0300

    deneme

--- app/src/main/java/com/example/luno/OracleDatabaseHelper.java
+++ app/src/main/java/com/example/luno/OracleDatabaseHelper.java
@@ -2,22 +2,52 @@ package com.example.luno;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.EditText;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 public class OracleDatabaseHelper {
-     //AsyncTask kullanarak ana iş parçacığını bloke etmeden veri tabanına ulaşıyoruz ve LUNO_USER tablosuna kullanıcıdan aldığımnız verileri ekliyoruz.
-    public static void VeriADD(final Context context, final String username, final String password) {
+    //AsyncTask kullanarak ana iş parçacığını bloke etmeden veri tabanına ulaşıyoruz ve LUNO_USER tablosuna kullanıcıdan aldığımnız verileri ekliyoruz.
+
+    public static Connection connection;
+
+    public static void DatabaseConnec() throws ClassNotFoundException, SQLException { //Database bağlantımızı bir method haline getirip her yerden erişelbilir elde etmiş oldukç
         AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                try {
+                    Class.forName("oracle.jdbc.driver.OracleDriver");
+                    connection = DriverManager.getConnection("jdbc:oracle:thin:@//192.168.1.106:1521/XEPDB1", "YASINS", "ys");
+                } catch (SQLException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                    // İşlem tamamlandıktan sonra yapılacak işlemler buraya yazılır
+                }
+            }
+
+            protected void onPostExecutes(Void aVoid) {
+
+            }
+
+        }.execute();
+
+    }
+
+    public static void UserAdd(final Context context, final String username, final String password) {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+
             @Override
             protected Void doInBackground(Void... voids) {
                 try {
                     // Veritabanı işlemleri
-                    Class.forName("oracle.jdbc.driver.OracleDriver");
-                    Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//192.168.1.104:1521/XEPDB1", "YASINS", "ys");
+                    DatabaseConnec();
                     String sql = "INSERT INTO LUNO_USER (NAME,PASSWORD) VALUES (?,?)";
                     PreparedStatement preparedStatement = connection.prepareStatement(sql);
                     preparedStatement.setString(1, username);
@@ -38,7 +68,86 @@ public class OracleDatabaseHelper {
         };
         task.execute();
     }
+
+    public static void PlayerNameAdd(final Context context, final String playername) {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    // Veritabanı işlemleri
+                    DatabaseConnec();
+                    String sql = "INSERT INTO PLAYER (NAME) VALUES (?)";
+                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, playername);
+                    int affectedRows = preparedStatement.executeUpdate();
+                    preparedStatement.close();
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+        task.execute();
+    }
+
+
+    // Örnek bir metot
+    public static void checkPlayerLogin(final String username,final OnLoginCheckedListener listener ) {
+        // AsyncTask'ı başlatmak için yeni bir instance oluştur ve execute() metoduyla çalıştır
+        AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+
+
+
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+               boolean result = false;
+
+                try {
+                    DatabaseConnec();
+                    String sql = "SELECT COUNT(*) FROM PLAYER WHERE NAME = ? ";
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    statement.setString(1, username);
+                    ResultSet resultSet = statement.executeQuery();
+                    while (resultSet.next()) {
+                        // Sonuçları işleyin
+                        result = true;
+                    }
+
+                }catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+
+                } finally {
+                    try {
+                        connection.close();
+                    } catch (SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    return result;
+
+                }
+
+            }
+            @Override
+            protected void onPostExecute(Boolean result){
+                super.onPostExecute(result);
+                listener.onLoginChecked(result);
+            }
+        };
+        task.execute();
+
+
+    }
+    public interface OnLoginCheckedListener {
+        void onLoginChecked(boolean result);
+    }
+
 }
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/luno/RegisterActivity.java
+++ app/src/main/java/com/example/luno/RegisterActivity.java
@@ -1,14 +1,20 @@
 package com.example.luno;
 
+import static java.sql.DriverManager.getConnection;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class RegisterActivity extends AppCompatActivity {
-    private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,32 +26,65 @@ public class RegisterActivity extends AppCompatActivity {
         EditText PlayerName = findViewById(R.id.Player_Name);
         EditText userName = findViewById(R.id.UserNameSubmit);
         EditText userPassword = findViewById(R.id.Password);
-        EditText userPasswordSubmit = findViewById(R.id.Password_submit);
+        EditText userPC = findViewById(R.id.Password_submit);
+
         Submit.setOnClickListener(new View.OnClickListener() { //Kayıt ol butonuna tıklandığı an çalışcak işlemler
             @Override
             public void onClick(View v) {
-              String uName=userName.getText().toString();
-              String uPassword=userPassword.getText().toString();
-              if(8>uPassword.length()){ //eğer şifre 8 karakter kısalığından kısaysa hata mesajı verdirip geri döndürüyoruz
-                    CustomDialog dialog = new CustomDialog(RegisterActivity.this,"Hata","Şifreniz en az 8 karakterli olmak zorundadır.");
+
+                String uName = userName.getText().toString();
+                String userpass = userPC.getText().toString();
+                String uPassword = userPassword.getText().toString();
+                String pName = PlayerName.getText().toString();
+
+                boolean contorlPassword = uPassword.equals(userpass);
+                if (8 > uPassword.length() && contorlPassword == false) { //eğer şifre 8 karakter kısalığından kısaysa hata mesajı verdirip geri döndürüyoruz
+                    CustomDialog dialog = new CustomDialog(RegisterActivity.this, "Hata", "Şifreniz en az 8 karakterli olmak zorundadır ve lütfen şifreleriniz aynı olsun.");
                     dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg);
                     dialog.show();
-              }else {
-                  OracleDatabaseHelper.VeriADD(RegisterActivity.this,uName,uPassword); //Burada ise database işlemlerimizi yaptırıyoruz...
-              }
+                } else {
 
+                    OracleDatabaseHelper.UserAdd(RegisterActivity.this, uName, uPassword);
+                    OracleDatabaseHelper.PlayerNameAdd(RegisterActivity.this, pName);
+                    if (PlayerName.getText().toString().matches("")) {
+                        CustomDialog dialog = new CustomDialog(RegisterActivity.this, "Hata", "Kullanıcı adı boş geçilemez. Lütfen kullanıcı adınızı giriniz.");
+                        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg);
+                        dialog.show();
+
+
+                    } else {
+                        OracleDatabaseHelper.checkPlayerLogin(pName, new OracleDatabaseHelper.OnLoginCheckedListener() {
+                            @Override
+                            public void onLoginChecked(boolean result) {
+
+
+                                if (result == true) {
+                                    CustomDialog dialog = new CustomDialog(RegisterActivity.this, "Hata", "Böyle bir kullanıcı adı zaten var lütfen başka bir kullanıcı adı oluşturunuz...");
+                                    dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg);
+                                    dialog.show();
+
+
+                                } else {
+                                    OracleDatabaseHelper.PlayerNameAdd(RegisterActivity.this, pName);
+                                }
+
+
+                            }
+
+                        });
+
+
+                    }
+
+                }
 
             }
 
         });
-
     }
-
-
-
-
-
-
-
 }
+
+
+
+
 
