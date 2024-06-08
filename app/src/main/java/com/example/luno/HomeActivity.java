9955499127b3d2a1d0a4f5154d4186f5c3e38672
yasin <yasinsivis9@gmail.com>
2024-06-08 21:33:26 +0300

    Buttonlar adlandırıldı. Button Tagleri oluşturuldu.

--- app/src/main/java/com/example/luno/HomeActivity.java
+++ app/src/main/java/com/example/luno/HomeActivity.java
@@ -14,6 +14,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeActivity extends AppCompatActivity {
+    Button btnDOG;
+    Button btnCAT;
+    Button btnBUG;
+    Button btnDUCK;
+    Button btnTIGER;
+    Button btnLION;
+    Button btnDOLPHIN;
+    Button btnWOLF;
+    Button btnSQUIRREL;
+    Button btnTURTLE;
+    Button btnBIRD;
+    Button btnPARROT;
+    Button btnPIG;
+    Button btnBULL;
+    Button btnBEAR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +36,47 @@ public class HomeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
+        //tanımlar
+        btnDOG=findViewById(R.id.DOG);
+        btnCAT=findViewById(R.id.CAT);
+        btnBEAR=findViewById(R.id.BEAR);
+        btnBUG=findViewById(R.id.BUG);
+        btnDUCK=findViewById(R.id.DUCK);
+        btnLION=findViewById(R.id.LION);
+        btnTIGER=findViewById(R.id.TIGER);
+        btnWOLF=findViewById(R.id.WOLF);
+        btnDOLPHIN=findViewById(R.id.DOLPHIN);
+        btnPARROT=findViewById(R.id.PARROT);
+        btnBIRD=findViewById(R.id.BIRD);
+        btnPIG=findViewById(R.id.PIG);
+        btnSQUIRREL=findViewById(R.id.SQUIRREL);
+        btnTURTLE=findViewById(R.id.TURTLE);
+        btnBULL=findViewById(R.id.BULL);
 
+        //tags
+        btnDOG.setTag(0);
+        btnCAT.setTag(1);
+        btnDUCK.setTag(2);
+        btnBUG.setTag(3);
+        btnBIRD.setTag(4);
+        btnTURTLE.setTag(5);
+        btnPARROT.setTag(6);
+        btnSQUIRREL.setTag(7);
+        btnBULL.setTag(8);
+        btnPIG.setTag(9);
+        btnTIGER.setTag(10);
+        btnLION.setTag(11);
+        btnDOLPHIN.setTag(12);
+        btnWOLF.setTag(13);
+        btnBEAR.setTag(14);
     };
     public void PlayBTN (View view){
-        Intent QuestionUp = new Intent(HomeActivity.this,QuestionActivity.class);
-        startActivity(QuestionUp);
+
+        int testId=(int) view.getTag();
+        Intent intent = new Intent(HomeActivity.this, QuestionActivity.class);
+        intent.putExtra("Index", testId);
+        startActivity(intent);
+      
     }
 
 }
--- app/src/main/java/com/example/luno/QuestionActivity.java
+++ app/src/main/java/com/example/luno/QuestionActivity.java
@@ -1,15 +1,16 @@
 package com.example.luno;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuestionActivity extends AppCompatActivity {
-    private  TextView text, Hint, Point;
-    private RadioButton rb1, rb2, rb3, rb4;
+
 
 
 
@@ -19,12 +20,24 @@ public class QuestionActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_question);
 
-        text = findViewById(R.id.questionTextView);
-        rb1 = findViewById(R.id.answer1RadioButton);
-        rb2=findViewById(R.id.answer2RadioButton);
-        rb3=findViewById(R.id.answer3RadioButton);
-        rb4=findViewById(R.id.answer4RadioButton);
-        Hint = findViewById(R.id.Hint);
+        int Index = getIntent().getIntExtra("Index", -1);
+        Log.d("QUESTiıonj","Rreceived  index:" + Index);
+        int index = APP.TestList.indexOf(0);
+        Log.d("Question","Index of 0 in TextList : "+index);
+        Question question = new Question();
+        RadioButton r1=findViewById(R.id.answer1RadioButton);
+
+
+        switch (Index){
+            case 0:
+                Toast.makeText(this, "DOG butonuna tıklandı", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, "DOG butonuna tıklandı", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
     }
 
 }
